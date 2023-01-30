b67e5b76a044257fa451b97e78a87ca699be3275
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-29 21:19:58 -0800

    working on get/post

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const { readFromFile, readAndAppend } = require('./helperFunctions/fsUtils')
 
 const app = express();
 
@@ -10,11 +12,12 @@ app.use(express.static('public'));
 
 // homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 
